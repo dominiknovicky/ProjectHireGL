e95695d0fc5b5798ff8408c6e2305db97dbaa7ba
Dominik <dominik.novicky24@gmail.com>
2018-02-01 13:25:06 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,8 +1,27 @@
 (function($){
 
-	var winWidth = ($("#window").css("height"));
-	console.log(winWidth);
+	$("#logIn").click(function(){
+		var error = false;
+		var userNameInput = $("#userNameInput").val();
+		var passwordInput = $("#passwordInput").val();
 
+		
+		if(userNameInput.trim().length < 2 || passwordInput.trim().length < 2){	
+			$("#error").html("Invalid Username or Password.<br>Please try again.");
+			error = true;
+		}
+		else {
+			$("#error").html("");
+		}
 
-
+		$("userNameInput, passwordInput").keyup(function(){
+			if(userNameInput.trim().length < 2 || passwordInput.trim.length < 2) {	
+				$("#error").html("Invalid Username or Password.<br>Please try again.");
+				error = true;
+			}
+			else {
+				$("#error").html("");
+			}
+		});	
+	});
 })(jQuery);
